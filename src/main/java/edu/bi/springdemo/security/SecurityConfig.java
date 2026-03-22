7e0e64f67440885e933dde6a2e348a1210c40476
emilia <emilia.mykowska@gmail.com>
2026-03-22 18:34:16 +0100

    disable csrf; change accessibility of users

--- src/main/java/edu/bi/springdemo/security/SecurityConfig.java
+++ src/main/java/edu/bi/springdemo/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package edu.bi.springdemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,12 +23,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.addFilterBefore(new JWTTokenFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
+        return http.csrf(csrf -> csrf.disable())
+                .addFilterBefore(new JWTTokenFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                         authorizationManagerRequestMatcherRegistry ->
                                 authorizationManagerRequestMatcherRegistry
                                         .requestMatchers("/login").permitAll()
-                                        .requestMatchers("/books/**").hasAnyRole("READER", "LIBRARIAN")
+                                        .requestMatchers(HttpMethod.GET, "/books").hasAnyRole("READER", "LIBRARIAN")
+                                        .requestMatchers("/reviews").hasAnyRole("READER", "LIBRARIAN")
                                         .requestMatchers("/**").hasRole("LIBRARIAN")
                 ).sessionManagement(httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer

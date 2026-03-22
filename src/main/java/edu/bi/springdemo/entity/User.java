66ab0e7b7281d2ccf665597d8ebcf62f503d8d56
emilia <emilia.mykowska@gmail.com>
2026-03-22 18:34:46 +0100

    add username restriction

--- src/main/java/edu/bi/springdemo/entity/User.java
+++ src/main/java/edu/bi/springdemo/entity/User.java
@@ -10,6 +10,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer userId;
 
+    @Column(unique = true)
     private String username;
     private String password;
 

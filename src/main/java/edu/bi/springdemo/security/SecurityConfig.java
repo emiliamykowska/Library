38019ab9eb947aeda1db60f165cfb470d0fbce3f
emilia <emilia.mykowska@gmail.com>
2026-03-29 20:34:52 +0200

    add access denied handling in SecurityConfig

--- src/main/java/edu/bi/springdemo/security/SecurityConfig.java
+++ src/main/java/edu/bi/springdemo/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,46 +14,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 // ---JWT---
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    private final JWTTokenService jwtTokenService;
-//
-//    @Autowired
-//    public SecurityConfig(JWTTokenService jwtTokenService){
-//        this.jwtTokenService = jwtTokenService;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http.csrf(AbstractHttpConfigurer::disable)
-//                .cors(AbstractHttpConfigurer::disable)
-//                .addFilterBefore(new JWTTokenFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
-//                .authorizeHttpRequests(
-//                        authorizationManagerRequestMatcherRegistry ->
-//                                authorizationManagerRequestMatcherRegistry
-//                                        .requestMatchers("/login").permitAll()
-//                                        .requestMatchers(HttpMethod.GET, "/books").hasAnyRole("READER", "LIBRARIAN")
-//                                        .requestMatchers("/reviews").hasAnyRole("READER", "LIBRARIAN")
-//                                        .requestMatchers("/**").hasRole("LIBRARIAN")
-//                ).sessionManagement(httpSecuritySessionManagementConfigurer ->
-//                        httpSecuritySessionManagementConfigurer
-//                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .build();
-//    }
-//}
-
-// ---SESSION---
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
+
+    private final JWTTokenService jwtTokenService;
+
+    @Autowired
+    public SecurityConfig(JWTTokenService jwtTokenService){
+        this.jwtTokenService = jwtTokenService;
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JWTTokenFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                         authorizationManagerRequestMatcherRegistry ->
                                 authorizationManagerRequestMatcherRegistry
@@ -61,9 +38,41 @@ public class SecurityConfig{
                                         .requestMatchers("/reviews").hasAnyRole("READER", "LIBRARIAN")
                                         .requestMatchers("/**").hasRole("LIBRARIAN")
                 )
+                .exceptionHandling(e ->
+                        e.accessDeniedHandler(((request, response, accessDeniedException) -> {
+                                    response.setStatus(HttpStatus.FORBIDDEN.value()); //code 403
+                                    response.getWriter().write("""
+                                    {"message": "Access denied, your role is insufficient"}""");
+                                })))
+
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer
-                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)) //changed from stateless, so session can store state
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 }
+
+// ---SESSION---
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig{
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//        return http.csrf(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(
+//                        authorizationManagerRequestMatcherRegistry ->
+//                                authorizationManagerRequestMatcherRegistry
+//                                        .requestMatchers("/login").permitAll()
+//                                        .requestMatchers(HttpMethod.GET, "/books").hasAnyRole("READER", "LIBRARIAN")
+//                                        .requestMatchers("/reviews").hasAnyRole("READER", "LIBRARIAN")
+//                                        .requestMatchers("/**").hasRole("LIBRARIAN")
+//                )
+//                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+//                        httpSecuritySessionManagementConfigurer
+//                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)) //changed from stateless, so session can store state
+//                .build();
+//    }
+//}

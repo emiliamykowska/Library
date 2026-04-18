459982b6db1c20007921b66d471be48aba43947a
emilia <emilia.mykowska@gmail.com>
2026-04-18 14:20:24 +0200

    delete unnecessary files

--- src/main/java/edu/bi/springdemo/repository/BookDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.bi.springdemo.repository;
-
-import edu.bi.springdemo.entity.BookDetail;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookDetailRepository extends CrudRepository<BookDetail, Integer> {
-}
